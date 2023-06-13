fddddba7b9903ce444fa58aaa699e66a35702413
Harry <skswls0@daum.net>
2023-06-13 17:58:09 +0900

    프로젝트 생성 및 초기 세팅

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import HookMqtt from './components/Hook/';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <HookMqtt />
+      {/* Hook or Class */}
+      {/* <ClassMqtt /> */}
+    </div>
+  );
+}
+
+export default App;
