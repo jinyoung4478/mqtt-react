db7f0e9d5f404aa5e40bfaf835eeddcd6211dfe2
김진영 <skswls0@daum.net>
2023-06-17 16:03:13 +0900

    gh-pages 배포를 위한 설정 변경

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,10 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import {NodeGlobalsPolyfillPlugin} from '@esbuild-plugins/node-globals-polyfill';
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 100000000,
-    rollupOptions: {
-      output: {
-        dir: 'build',
-      },
-    },
-  },
-  // ...other config settings
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-        // react(),
-      ],
-    },
-  },
+   base: '/mqtt-react',
+   plugins: [react()],
+   build: {
+      chunkSizeWarningLimit: 100000000,
+   },
 });
